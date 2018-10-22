9f5b33f8d959a70111d487b008a6f67da0e12c40
stevengum <14935595+stevengum@users.noreply.github.com>
2018-10-22 10:42:36 -0700

    trivial cleanup in auth tests

--- libraries/botframework-connector/tests/auth.test.js
+++ libraries/botframework-connector/tests/auth.test.js
@@ -12,7 +12,7 @@ describe('Bot Framework Connector - Auth Tests', function () {
             it('with correct ChannelId should validate', function (done) {
                 Connector.ChannelValidation.ToBotFromChannelTokenValidationParameters.ignoreExpiration = true;
                 var header = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IkdDeEFyWG9OOFNxbzdQd2VBNy16NjVkZW5KUSIsIng1dCI6IkdDeEFyWG9OOFNxbzdQd2VBNy16NjVkZW5KUSJ9.eyJzZXJ2aWNldXJsIjoiaHR0cHM6Ly93ZWJjaGF0LmJvdGZyYW1ld29yay5jb20vIiwiaXNzIjoiaHR0cHM6Ly9hcGkuYm90ZnJhbWV3b3JrLmNvbSIsImF1ZCI6IjM5NjE5YTU5LTVhMGMtNGY5Yi04N2M1LTgxNmM2NDhmZjM1NyIsImV4cCI6MTUxNjczNzUyMCwibmJmIjoxNTE2NzM2OTIwfQ.TBgpxbDS-gx1wm7ldvl7To-igfskccNhp-rU1mxUMtGaDjnsU--usH4OXZfzRsZqMlnXWXug_Hgd_qOr5RH8wVlnXnMWewoZTSGZrfp8GOd7jHF13Gz3F1GCl8akc3jeK0Ppc8R_uInpuUKa0SopY0lwpDclCmvDlz4PN6yahHkt_666k-9UGmRt0DDkxuYjbuYG8EDZxyyAhr7J6sFh3yE2UGRpJjRDB4wXWqv08Cp0Gn9PAW2NxOyN8irFzZH5_YZqE3DXDAYZ_IOLpygXQR0O-bFIhLDVxSz6uCeTBRjh8GU7XJ_yNiRDoaby7Rd2IfRrSnvMkBRsB8MsWN8oXg';
-                var credentials = new Connector.SimpleCredentialProvider('39619a59-5a0c-4f9b-87c5-816c648ff357', '');
+                var credentials = new SimpleCredentialProvider('39619a59-5a0c-4f9b-87c5-816c648ff357', '');
                 Connector.JwtTokenValidation.validateAuthHeader(header, credentials, undefined, 'webchat', 'https://webchat.botframework.com/')
                     .then(claims => {
                         assert(claims.isAuthenticated);
@@ -27,7 +27,7 @@ describe('Bot Framework Connector - Auth Tests', function () {
             it('with incorrect ChannelId should not validate', function (done) {
                 Connector.ChannelValidation.ToBotFromChannelTokenValidationParameters.ignoreExpiration = true;
                 var header = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IkdDeEFyWG9OOFNxbzdQd2VBNy16NjVkZW5KUSIsIng1dCI6IkdDeEFyWG9OOFNxbzdQd2VBNy16NjVkZW5KUSJ9.eyJzZXJ2aWNldXJsIjoiaHR0cHM6Ly93ZWJjaGF0LmJvdGZyYW1ld29yay5jb20vIiwiaXNzIjoiaHR0cHM6Ly9hcGkuYm90ZnJhbWV3b3JrLmNvbSIsImF1ZCI6IjM5NjE5YTU5LTVhMGMtNGY5Yi04N2M1LTgxNmM2NDhmZjM1NyIsImV4cCI6MTUxNjczNzUyMCwibmJmIjoxNTE2NzM2OTIwfQ.TBgpxbDS-gx1wm7ldvl7To-igfskccNhp-rU1mxUMtGaDjnsU--usH4OXZfzRsZqMlnXWXug_Hgd_qOr5RH8wVlnXnMWewoZTSGZrfp8GOd7jHF13Gz3F1GCl8akc3jeK0Ppc8R_uInpuUKa0SopY0lwpDclCmvDlz4PN6yahHkt_666k-9UGmRt0DDkxuYjbuYG8EDZxyyAhr7J6sFh3yE2UGRpJjRDB4wXWqv08Cp0Gn9PAW2NxOyN8irFzZH5_YZqE3DXDAYZ_IOLpygXQR0O-bFIhLDVxSz6uCeTBRjh8GU7XJ_yNiRDoaby7Rd2IfRrSnvMkBRsB8MsWN8oXg';
-                var credentials = new Connector.SimpleCredentialProvider('39619a59-5a0c-4f9b-87c5-816c648ff357', '');
+                var credentials = new SimpleCredentialProvider('39619a59-5a0c-4f9b-87c5-816c648ff357', '');
                 Connector.JwtTokenValidation.validateAuthHeader(header, credentials, undefined, 'foo', 'https://webchat.botframework.com/')
                     .then(claims => done(new Error('Expected validation to fail.')))
                     .catch(err => done())
@@ -37,10 +37,10 @@ describe('Bot Framework Connector - Auth Tests', function () {
             });
 
             xit('with correct AppId and ServiceUrl should validate', function (done) {
-                var tokenGenerator = new Connector.MicrosoftAppCredentials('2cd87869-38a0-4182-9251-d056e8f0ac24', '2.30Vs3VQLKt974F');
+                var tokenGenerator = new MicrosoftAppCredentials('2cd87869-38a0-4182-9251-d056e8f0ac24', '2.30Vs3VQLKt974F');
                 tokenGenerator.getToken(true).then(token => {
                     var header = `Bearer ${ token }`;
-                    var credentials = new Connector.SimpleCredentialProvider('2cd87869-38a0-4182-9251-d056e8f0ac24', '');
+                    var credentials = new SimpleCredentialProvider('2cd87869-38a0-4182-9251-d056e8f0ac24', '');
                     Connector.JwtTokenValidation.validateAuthHeader(header, credentials, undefined, '', 'https://webchat.botframework.com/')
                         .then(claims => {
                             assert(claims.isAuthenticated);
@@ -52,10 +52,10 @@ describe('Bot Framework Connector - Auth Tests', function () {
             });
 
             xit('with BotAppId differs should not validate', function (done) {
-                var tokenGenerator = new Connector.MicrosoftAppCredentials('2cd87869-38a0-4182-9251-d056e8f0ac24', '2.30Vs3VQLKt974F');
+                var tokenGenerator = new MicrosoftAppCredentials('2cd87869-38a0-4182-9251-d056e8f0ac24', '2.30Vs3VQLKt974F');
                 tokenGenerator.getToken(true).then(token => {
                     var header = `Bearer ${ token }`;
-                    var credentials = new Connector.SimpleCredentialProvider('00000000-0000-0000-0000-000000000000', '');
+                    var credentials = new SimpleCredentialProvider('00000000-0000-0000-0000-000000000000', '');
                     Connector.JwtTokenValidation.validateAuthHeader(header, credentials, undefined, '', '')
                         .then(claims => done(new Error('Expected validation to fail.')))
                         .catch(err => {
@@ -66,10 +66,10 @@ describe('Bot Framework Connector - Auth Tests', function () {
             });
 
             xit('with noCredentials should not validate', function (done) {
-                var tokenGenerator = new Connector.MicrosoftAppCredentials('2cd87869-38a0-4182-9251-d056e8f0ac24', '2.30Vs3VQLKt974F');
+                var tokenGenerator = new MicrosoftAppCredentials('2cd87869-38a0-4182-9251-d056e8f0ac24', '2.30Vs3VQLKt974F');
                 tokenGenerator.getToken(true).then(token => {
                     var header = `Bearer ${ token }`;
-                    var credentials = new Connector.SimpleCredentialProvider('', '');
+                    var credentials = new SimpleCredentialProvider('', '');
                     Connector.JwtTokenValidation.validateAuthHeader(header, credentials, undefined, '', '')
                         .then(claims => done(new Error('Expected validation to fail.')))
                         .catch(err => {
